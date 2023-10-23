2ea20380b569f86da9d3c687c3303d495adc87cf
Amy Haywood Dutton <me@amyhaywood.com>
2023-10-23 11:20:00 -0500

    💄 Adjusted page header styles

--- web/src/components/PageHeading/PageHeading.tsx
+++ web/src/components/PageHeading/PageHeading.tsx
@@ -1,10 +1,23 @@
+import { Link } from '@redwoodjs/router'
+
+import Icon from '../Icon/Icon'
+
 interface PageHeadingProps {
+  backButtonLink: string
   heading: string
 }
 
-const PageHeading = ({ heading }: PageHeadingProps) => {
+const PageHeading = ({ heading, backButtonLink = '' }: PageHeadingProps) => {
   return (
-    <div className="border-b-2 border-b-black pb-4 pl-36 pt-14">
+    <div className="relative border-b-2 border-b-black bg-black pb-4 pl-16 pt-14 text-white">
+      {backButtonLink && (
+        <Link
+          to={backButtonLink}
+          className="absolute left-5 top-[58px] transition-all hover:left-4"
+        >
+          <Icon id="arrow" />
+        </Link>
+      )}
       <h1 className="text-xl font-bold">{heading}</h1>
     </div>
   )
