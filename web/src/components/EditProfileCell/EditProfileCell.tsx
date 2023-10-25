e24392df7dacb9086236e0b560755743f8b9c13d
Amy Haywood Dutton <me@amyhaywood.com>
2023-10-24 21:59:41 -0500

    ✨ Can update the profile text fields

--- web/src/components/EditProfileCell/EditProfileCell.tsx
+++ web/src/components/EditProfileCell/EditProfileCell.tsx
@@ -3,8 +3,19 @@ import type {
   FindEditProfileQueryVariables,
 } from 'types/graphql'
 
-import { Form, TextAreaField, TextField, UrlField } from '@redwoodjs/forms'
-import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+import {
+  Form,
+  TextAreaField,
+  TextField,
+  UrlField,
+  useForm,
+} from '@redwoodjs/forms'
+import {
+  type CellSuccessProps,
+  type CellFailureProps,
+  useMutation,
+} from '@redwoodjs/web'
+import { toast } from '@redwoodjs/web/dist/toast'
 
 import Avatar from '../Avatar/Avatar'
 import Icon from '../Icon/Icon'
@@ -12,12 +23,21 @@ import Icon from '../Icon/Icon'
 export const QUERY = gql`
   query FindEditProfileQuery($id: Int!) {
     user(id: $id) {
+      id
       bio
       location
       website
       name
       avatar
       cover
+    }
+  }
+`
+
+const UPDATE_PROFILE_MUTATION = gql`
+  mutation UpdateProfileMutation($id: Int!, $input: UpdateUserInput!) {
+    updateUser(id: $id, input: $input) {
+      id
     }
   }
 `
@@ -35,9 +55,34 @@ export const Failure = ({
 export const Success = ({
   user,
 }: CellSuccessProps<FindEditProfileQuery, FindEditProfileQueryVariables>) => {
+  const formMethods = useForm()
+
+  const [updateProfile, { loading }] = useMutation(UPDATE_PROFILE_MUTATION, {
+    onCompleted: () => {
+      toast.success('Profile updated.')
+    },
+    onError: (error) => {
+      console.error(error)
+      toast.error(error.message)
+    },
+  })
+
+  const handleCancel = (event) => {
+    event.preventDefault()
+    formMethods.reset()
+  }
+
+  const handleSubmit = (data) => {
+    updateProfile({ variables: { id: user.id, input: data } })
+  }
+
   return (
-    <Form className="big-form">
-      <fieldset>
+    <Form
+      className="big-form"
+      formMethods={formMethods}
+      onSubmit={handleSubmit}
+    >
+      <fieldset disabled={loading}>
         <div className="relative mb-[120px]">
           {user.cover ? (
             <img src={user.cover} alt={user.name} />
@@ -53,12 +98,12 @@ export const Success = ({
             </button>
           </div>
           <div className="absolute -bottom-20 left-12 rounded-full border-4 border-white">
-            <Avatar size="large" />
-            <div className="center absolute inset-0 h-full w-full gap-3">
-              <button className="center h-[54px] w-[54px] rounded-full bg-hotMagenta text-white">
+            <Avatar size="large" image={user.avatar} />
+            <div className="center absolute inset-0 h-full w-full gap-3 rounded-full bg-black bg-opacity-60">
+              <button className="center h-[54px] w-[54px] rounded-full bg-hotMagenta text-white hover:bg-black">
                 <Icon id="camera" size={36} />
               </button>
-              <button className="center h-[54px] w-[54px] rounded-full bg-white text-black">
+              <button className="center h-[54px] w-[54px] rounded-full bg-white text-black hover:bg-black hover:text-white">
                 <Icon id="close" size={36} />
               </button>
             </div>
@@ -82,6 +127,9 @@ export const Success = ({
             <UrlField name="website" defaultValue={user.website} />
           </div>
           <div className="flex justify-end">
+            <button className="button no-bg" onClick={handleCancel}>
+              Cancel
+            </button>
             <button className="button solid">Update Profile</button>
           </div>
         </div>
